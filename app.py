ec9813e6478386138884ccecdc43486fe2cfe8c5
MasonDill <mjd159@pitt.edu>
2023-10-11 04:09:11 +0000

    IL support added

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
+from flask import Flask, request, render_template
+import os
+import sys
+from datetime import datetime
+import subprocess
+
 MODEL_PATH = "/models/semantic_model.meta"
 CTC_PREDICT_PATH = "/tf-end-to-end/ctc_predict.py"
 S2M_PATH = "/semantic_to_mei/semantic_to_mei.py"
 PYTHON_PATH = "/usr/bin/python"
+PYTHON3_PATH = "/usr/bin/python3"
 VOCAB_PATH = "/tf-end-to-end/Data/vocabulary_semantic.txt"
 
 SEMANTIC_PATH = "/temp/semantic/"
 IL_PATH = "/temp/IL/"
 OUT_PATH = "/temp/out/"
-
-from flask import Flask, request, render_template
-import os
-import sys
-import datetime
-import asyncio
-import subprocess
 
 app = Flask(__name__)
 
@@ -46,9 +46,13 @@ def upload_file():
     file.save(input_file_path)
 
     # generate the output file
-    output_file_path = image_to_out(input_file_path, 'xml', 'il')
-
+    results = image_to_out(input_file_path, 'musicxml', 'il')
+    print(results)
     return 'File uploaded successfully', 200
+
+def get_timestamp():
+    date_string =  datetime.now().strftime("%Y%m%d%H%M%S")
+    return ''.join(filter(str.isdigit, date_string))
 
 def predict(image_path):
     #check that the constant file paths exist
@@ -65,13 +69,12 @@ def predict(image_path):
     if not os.path.exists(image_path):
         return 'Image file does not exist', 400
     
-    current_ts = datetime.datetime.now().timestamp()
+    current_ts = get_timestamp()
     semantic_output_file = SEMANTIC_PATH + "temp" +current_ts + ".semantic"
     
     #run the prediction
-    command = PYTHON_PATH + " " + CTC_PREDICT_PATH + " " + MODEL_PATH + " " + image_path + " " + VOCAB_PATH + " > " + semantic_output_file
-    process =  os.system(command)
-    process.wait()
+    command = PYTHON_PATH + " " + CTC_PREDICT_PATH + " -model " + MODEL_PATH + " -image " + image_path + " -vocabulary " + VOCAB_PATH + " > " + semantic_output_file
+    subprocess.call(command, shell=True)
 
     return semantic_output_file
 
@@ -83,11 +86,11 @@ def semantic_to_IL(semantic_path, il):
     if not os.path.exists(PYTHON_PATH):
         return 'Python path does not exist', 400
     
-    current_ts = datetime.datetime.now().timestamp()
-    il_output_file = IL_PATH + "temp" +current_ts + "." + il
-    command = PYTHON_PATH + " " + S2M_PATH + " " + semantic_path + " " + il_output_file
-    process = os.system(command)
-    process.wait()
+    current_ts = get_timestamp()
+    il_output_file = IL_PATH + "temp" +current_ts
+    print(il_output_file)
+    command = PYTHON3_PATH + " " + S2M_PATH + " " + semantic_path + " -type" +il +" -o " + il_output_file
+    subprocess.call(command, shell=True)
 
     return il_output_file
 
@@ -97,8 +100,8 @@ def IL_to_out(IL_file, IL, out_type):
     if not os.path.exists(PYTHON_PATH):
         return 'Python path does not exist', 400
     
-    timestamp = datetime.datetime.now().timestamp()
-    output_filepath = OUT_PATH + "temp" +timestamp +"." + out_type
+    timestamp = get_timestamp()
+    output_filepath = OUT_PATH + "temp" +timestamp
 
     #TODO: run the image to out command
 
@@ -107,9 +110,11 @@ def IL_to_out(IL_file, IL, out_type):
 def image_to_out(image_path, il, out_type):
     semantic_file_path = predict(image_path)
     il_file_path = semantic_to_IL(semantic_file_path, il)
-    output_file_path = IL_to_out(il_file_path, il, out_type)
+    print("il file: " +il_file_path)
+    # output_file_path = IL_to_out(il_file_path, il, out_type)
+    # print("output file: " +output_file_path)
 
-    return (output_file_path, semantic_file_path, il_file_path)
+    return (semantic_file_path)
 
 if __name__ == '__main__':
     app.run(port=8890, debug=True)
---
+++ dockerfile
@@ -0,0 +1,10 @@
+FROM mason77804/monophonic:1-1
+#Add the webserver git repo
+#git clone https://github.com/MasonDill/mono-web.git
+#Get the il conversion repo
+#git clone https://github.com/MasonDill/semantic_to_mei.git
+#Install pip for python3
+#wget https://bootstrap.pypa.io/pip/3.5/get-pip.py
+#python3 get-pip.py
+#Install music21
+#python3 -m pip install music21
