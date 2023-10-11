33864321463f2cb3a5a1ec5154f8228c1223ff40
MasonDill <mjd159@pitt.edu>
2023-10-11 02:43:35 +0000

    Added processing functions

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
+MODEL_PATH = "/models/semantic_model.meta"
+CTC_PREDICT_PATH = "/tf-end-to-end/ctc_predict.py"
+S2M_PATH = "/semantic_to_mei/semantic_to_mei.py"
+PYTHON_PATH = "/usr/bin/python"
+VOCAB_PATH = "/tf-end-to-end/Data/vocabulary_semantic.txt"
+
+SEMANTIC_PATH = "/temp/semantic/"
+IL_PATH = "/temp/IL/"
+OUT_PATH = "/temp/out/"
+
 from flask import Flask, request, render_template
 import os
+import sys
+import datetime
+import asyncio
+import subprocess
 
 app = Flask(__name__)
 
@@ -16,8 +30,6 @@ def upload_file():
 
     file = request.files['file']
 
-    print(file)
-
     if file.filename == '':
         return 'No selected file', 400
 
@@ -28,12 +40,76 @@ def upload_file():
     if not os.path.exists('uploads'):
         os.makedirs('uploads')
 
-    file.save(os.path.join('uploads', file.filename))
+    # save the file
+    input_file_path = os.path.join('uploads', file.filename)
+
+    file.save(input_file_path)
+
+    # generate the output file
+    output_file_path = image_to_out(input_file_path, 'xml', 'il')
 
     return 'File uploaded successfully', 200
 
+def predict(image_path):
+    #check that the constant file paths exist
+    if not os.path.exists(MODEL_PATH):
+        return 'Model path does not exist', 400
+    if not os.path.exists(CTC_PREDICT_PATH):
+        return 'CTC predict path does not exist', 400
+    if not os.path.exists(S2M_PATH):
+        return 'Semantic to MEI path does not exist', 400
+    if not os.path.exists(PYTHON_PATH):
+        return 'Python path does not exist', 400
+    if not os.path.exists(VOCAB_PATH):
+        return 'Vocabulary path does not exist', 400
+    if not os.path.exists(image_path):
+        return 'Image file does not exist', 400
+    
+    current_ts = datetime.datetime.now().timestamp()
+    semantic_output_file = SEMANTIC_PATH + "temp" +current_ts + ".semantic"
+    
+    #run the prediction
+    command = PYTHON_PATH + " " + CTC_PREDICT_PATH + " " + MODEL_PATH + " " + image_path + " " + VOCAB_PATH + " > " + semantic_output_file
+    process =  os.system(command)
+    process.wait()
 
+    return semantic_output_file
 
+def semantic_to_IL(semantic_path, il):
+    if not os.path.exists(semantic_path):
+        return 'Semantic file does not exist', 400
+    if not os.path.exists(S2M_PATH):
+        return 'Semantic to MEI path does not exist', 400
+    if not os.path.exists(PYTHON_PATH):
+        return 'Python path does not exist', 400
+    
+    current_ts = datetime.datetime.now().timestamp()
+    il_output_file = IL_PATH + "temp" +current_ts + "." + il
+    command = PYTHON_PATH + " " + S2M_PATH + " " + semantic_path + " " + il_output_file
+    process = os.system(command)
+    process.wait()
+
+    return il_output_file
+
+def IL_to_out(IL_file, IL, out_type):
+    if not os.path.exists(IL_file):
+        return 'IL file does not exist', 400
+    if not os.path.exists(PYTHON_PATH):
+        return 'Python path does not exist', 400
+    
+    timestamp = datetime.datetime.now().timestamp()
+    output_filepath = OUT_PATH + "temp" +timestamp +"." + out_type
+
+    #TODO: run the image to out command
+
+    return output_filepath
+
+def image_to_out(image_path, il, out_type):
+    semantic_file_path = predict(image_path)
+    il_file_path = semantic_to_IL(semantic_file_path, il)
+    output_file_path = IL_to_out(il_file_path, il, out_type)
+
+    return (output_file_path, semantic_file_path, il_file_path)
 
 if __name__ == '__main__':
     app.run(port=8890, debug=True)
--- monophonic_api.js
+++ monophonic_api.js
@@ -1,73 +1,75 @@
 path_to_model = "/models/semantic_model.meta"
 path_to_ctc_predict = "/tf-end-to-end/ctc_predict.py"
+path_to_s2m = "/semantic_to_mei/semantic_to_mei.py"
 python_path = "/usr/bin/python"
 vocabulary_path = "/tf-end-to-end/Data/vocabulary_semantic.txt"
-//Use Node js and listen to port 8889
-//This is the API for the monophonic synthesizer
+
+const OUTPUT_PATH = "/temp/out/"
+const IL_OUTPUT_PATH = "/temp/IL/"
+
 
 var express = require('express');
 var app = express();
-var multer = require('multer');
 var bodyParser = require('body-parser');
 var fs = require('fs');
 var path = require('path');
-var config = require("./config.json")
+var config = require("./config.json");
+var cors = require('cors')
 
-// Set up Multer to handle file uploads
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      // Specify the directory where uploaded files will be saved
-      cb(null, 'uploads/');
-    },
-    filename: (req, file, cb) => {
-      // Rename the uploaded file (you can customize the naming logic)
-      cb(null, Date.now() + '-' + file.originalname);
-    },
-  });
-const upload = multer({ storage: storage });
+const { toUnicode } = require('punycode');
+const { exec } = require('child_process');
+const fileUpload = require('express-fileupload');
 
-// Serve static files in the 'uploads' directory
-app.use(express.static('uploads'));
+//https://mono-web.dill.digital/index
 
-//Create a server
-port = 8890
-var server = app.listen(port, function () {
-    console.log('Server is listening on port ', port);
+function check_file_exists(file_path){
+  if (!fs.existsSync(file_paasth)) {
+    throw "File does not exist " + file_path;
+  }
 }
-);
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
 
-// Define a route to handle file uploads
-app.post('/upload', upload.single('file'), (req, res) => {
-    if (!req.file) {
-      return res.status(400).json({ message: 'No file uploaded.' });
+//Take an image file and turn it into a semantic file using the model
+async function predict(image_file_path){
+  check_file_exists(image_file_path);
+  check_file_exists(path_to_model);
+  check_file_exists(path_to_ctc_predict);
+  check_file_exists(python_path);
+  check_file_exists(vocabulary_path);
+  
+
+  command = python_path + " " +path_to_ctc_predict + " -image " +image_file_path +" -model "+path_to_model +" -vocabulary " +vocabulary_path + " > /log.txt"
+  console.log(command);
+}
+
+//Take a semantic file and turn it into a intermediate language
+async function semantic_to_IL(semantic_file_path, IL){
+  var il_output_file = "temp." + IL
+  il_output_file = IL_OUTPUT_PATH +il_output_file
+
+  //Use semantic_to_mei
+  command = `${python_path} ${path_to_s2m} ${semantic_file_path} +type ${IL} +o ${il_output_file}`
+
+   exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error}`);
+      return;
     }
-    //#python ctc_predict.py -image Data/Example/000051652-1_2_1.png -model Models/semantic_model.meta -vocabulary Data/vocabulary_semantic.txt
-    //ctc_predict with the uploaded file
-    command = python_path + " " +path_to_ctc_predict + " -image " +path_input +" -model "+path_to_model +" -vocabulary " +vocabulary_path + " > /log.txt"
-    console.log(command);
-
-    //turn the semantic into midi file
-
-    //return the midi file to the client
+  });
   
-    // You can perform further processing on the uploaded file here
-  
-    res.status(200).json({ message: 'File uploaded successfully.' });
-});
-  
+  return il_output_file;
+}
 
 
-// //Create a socket
-// var io = require('socket.io').listen(server);
+async function IL_to_out(IL_file, IL, out_type){
+  var output_filepath = OUTPUT_PATH + "temp." + out_type
+  //TODO
+  //var command = 
+  return output_filepath;
+} 
 
-// //Create a MIDI file
-// var midi = require('midi-file');
-// var output = midi.writer();
-
-// //Create a MIDI file
-// var midi = require('midi-file');
-// var output = midi.writer();
-
+async function image_to_out(image_file_path, IL, out_type){
+  semantic_file_path = await predict(image_file_path)
+  il_file_path = await semantic_to_IL(semantic_file_path, IL)
+  output_filepath = IL_to_out(il_file_path, IL, out_type)
+  return (semantic_file_path, il_file_path, output_filepath)
+}
